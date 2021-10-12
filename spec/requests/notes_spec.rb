6e192b71c7b438f8d66d4d506b0071fb3c73a18c
Arthur Fincham <arthurfincham@me.com>
2021-10-12 15:45:05 +0100

    add test for after sign in

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -4,9 +4,17 @@ require 'rails_helper'
 
 RSpec.describe 'Notes', type: :request do
   describe 'before sign in GET /index' do
-    it 'directs client to sign in' do
+    it 'redirects client to sign_in' do
       get '/notes/index'
       expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'after sign in GET /index' do
+    it 'can get index of notes' do
+      sign_in FactoryBot.create(:user)
+      get '/notes/index'
+      expect(response).to have_http_status(:success)
     end
   end
 end
