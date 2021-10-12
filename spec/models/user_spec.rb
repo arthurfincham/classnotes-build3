a5d99b57d47de0b0c22c4151d16605a50ade3288
Arthur Fincham <arthurfincham@me.com>
2021-10-12 11:26:36 +0100

    refactor user_spec to user user factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it "is valid with valid attributes" do
-    user = User.new(email: "test@example.com", username: "testuser", password: "password")
+    # user = User.new(email: "test@example.com", username: "testuser", password: "password")
+    user = create(:user)
     expect(user).to be_valid
   end
 
