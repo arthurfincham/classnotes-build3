aeabdeb56321ef66f5d6d75b9579bceb1a474e83
Arthur Fincham <arthurfincham@me.com>
2021-10-12 11:42:23 +0100

    comment out pending tests

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :note do
-    
+    title { "Test Title"}
+    date { "2021-01-12" }
+    description { "This is a test description written in factory bot."}
   end
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "#title" do
+    it { should validate_presence_of(:title) }
+  end
+
+  describe "#date" do
+    it { should validate_presence_of(:date) }
+  end
+
+  describe "#description" do
+    it { should validate_presence_of(:description) }
+  end
+
+  it 'is valid with valid attributes' do
+    note = create(:note)
+    expect(note).to be_valid
+  end
 end
