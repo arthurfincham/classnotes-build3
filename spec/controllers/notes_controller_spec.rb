110389d7823dc75a462ac93b83430aeea2332f8d
Arthur Fincham <arthurfincham@me.com>
2021-10-12 16:20:56 +0100

    add note controller spec

---
+++ spec/controllers/notes_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe NotesController, type: :controller do
+
+  before do
+    sign_in FactoryBot.create(:user)
+  end
+
+  context "GET #index" do
+    it "returns a success response" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context "GET #new" do
+    it "assigns a blank note to the view" do
+      get :new
+      expect(assigns(:note)).to be_a_new(Note)
+    end
+  end
+
+  context "POST #create" do
+    it "redirects to notes page" do
+      params = { title: "Test Note", date: "2021-12-31", description: "A test description for a note in controller spec." }
+      post :create, params: { note: params }
+      expect(response).to redirect_to(root_path)
+    end
+  end
+end
