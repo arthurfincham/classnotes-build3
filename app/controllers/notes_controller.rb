577853951c71322ec9afda83d4b5691fbe93eed2
Arthur Fincham <arthurfincham@me.com>
2021-10-12 14:53:14 +0100

    add index action to notes controller, view working

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,15 +2,17 @@
 
 class NotesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_notes
+
   def index; end
 
   private
 
   def set_notes
     @notes = if params[:tag]
-               current_user.notes.tagged_with(params[:tag]).order(note_date: :desc)
+               current_user.notes.tagged_with(params[:tag]).order(date: :desc)
              else
-               current_user.notes.order(note_date: :desc)
+               current_user.notes.order(date: :desc)
              end
   end
 end
