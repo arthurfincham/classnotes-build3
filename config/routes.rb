8521e7ad03d815d8185e096d9f235a270416c309
Arthur Fincham <arthurfincham@me.com>
2021-10-13 09:07:13 +0100

    add routes for tag path

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :notes, only: %i[new create edit update destroy]
   root to: 'notes#index'
+  get 'tags/:tag', to: 'notes#index', as: :tag
 end
