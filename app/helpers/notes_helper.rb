ee0b3f34b6e8353739fb1c411c4628e7e7491e36
Arthur Fincham <arthurfincham@me.com>
2021-10-13 09:08:03 +0100

    add helper methods for notes

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,4 +1,32 @@
-# frozen_string_literal: true
-
 module NotesHelper
+  def format_date(note)
+    note.date.strftime("#{note.date.day.ordinalize} %b %Y")
+  end
+
+  def short_format_date(note)
+    note.date.strftime("%a #{note.date.day.ordinalize} %b")
+  end
+
+  def table_header
+    url = request.path_info
+    if url.include? 'tags'
+      request[:tag].parameterize(separator: '_')
+    elsif url.include? 'notes/new'
+      'New Note'
+    elsif url.include? 'notes' && 'edit'
+      "##{@note.id}"
+    else
+      'All Notes'
+    end
+  end
+
+  def format_title(note)
+    note.title.parameterize(separator: '_').downcase if note.title
+  end
+
+  def format_tags(note)
+    note.tag_list.map do |tag|
+      tag.parameterize(separator: '_').downcase
+    end
+  end
 end
