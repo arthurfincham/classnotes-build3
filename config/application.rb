382733478f313c016d661a88e6448fe08eee84a3
Arthur Fincham <arthurfincham@me.com>
2021-10-13 08:24:52 +0100

    remove bug in app config for travis

--- config/application.rb
+++ config/application.rb
@@ -36,14 +36,6 @@ module ClassnotesBuild3
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    
-    config.generator do |generate|
-      generate.helper = false
-      generate.assets = false
-      generate.view_specs = false
-      generate.helper_specs = false
-      generate.routing_specs = false
-      generate.controller_specs = false
-    end
+  
   end
 end
