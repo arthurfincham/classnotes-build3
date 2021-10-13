9af17c80cb2996816914618196d66ccbd27d2ff2
Arthur Fincham <arthurfincham@me.com>
2021-10-13 09:11:10 +0100

    add js and stylesheet from CNB2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,19 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import "animate.css"
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+$(document).ready(function () {
+  $("tr[data-link]").click(function () {
+    window.location = $(this).data("link")
+  });
+
+});
