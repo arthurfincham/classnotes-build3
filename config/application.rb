4316e1d63365dfda54aabb6615e032a31029f47c
Arthur Fincham <arthurfincham@me.com>
2021-10-13 09:22:59 +0100

    config asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -2,20 +2,7 @@
 
 require_relative 'boot'
 
-require 'rails'
-# Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'active_storage/engine'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_mailbox/engine'
-require 'action_text/engine'
-require 'action_view/railtie'
-require 'action_cable/engine'
-require 'sprockets/railtie'
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -27,7 +14,8 @@ module ClassnotesBuild3
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
